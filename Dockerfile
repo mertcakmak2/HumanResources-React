dffcb0abdb6b8c47a7f6bd93ceaf4c699d8af1c8
mertcakmak2 <mertcakmak2@gmail.com>
2021-07-19 10:02:54 +0300

    nginx and production dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,24 @@
-FROM node:14-alpine AS development
-ENV NODE_ENV development
-
+FROM node:14-alpine AS builder
+ENV NODE_ENV production
+# Add a work directory
 WORKDIR /app
-
+# Cache and Install dependencies
 COPY package.json .
 COPY package-lock.json .
-RUN npm install
-
+RUN npm install --production
+# Copy app files
 COPY . .
+# Build the app
+RUN npm build
 
-EXPOSE 3000
-
-CMD [ "npm", "start" ]
+# Bundle static assets with nginx
+FROM nginx:1.21.0-alpine as production
+ENV NODE_ENV production
+# Copy built assets from builder
+COPY --from=builder /app/build /usr/share/nginx/html
+# Add your nginx.conf
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+# Expose port
+EXPOSE 80
+# Start nginx
+CMD ["nginx", "-g", "daemon off;"]
--- src/pages/JobAnnounce/JobAnnounceList/components/JobList.js
+++ src/pages/JobAnnounce/JobAnnounceList/components/JobList.js
@@ -3,16 +3,20 @@ import { List, Avatar, Button } from 'antd';
 
 export default function JobAnnounceList({ jobList }) {
 
+    const onSelectJob = (job) => {
+        alert(JSON.stringify(job))
+    }
+
     return (
         <List
             itemLayout="horizontal"
             dataSource={jobList}
-            renderItem={item => (
-                <List.Item actions={[<Button onClick={() => alert(item.jobPosition)} danger>Görüntüle</Button>]}>
+            renderItem={job => (
+                <List.Item actions={[<Button onClick={() => onSelectJob(job)} danger>Görüntüle</Button>]}>
                     <List.Item.Meta
                         avatar={<Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbfvty6OkyuZ4caDe2Gn4uQ8p3OWpT0wLtrQ&usqp=CAU" />}
-                        title={item.jobPosition}
-                        description={item.jobDescription}
+                        title={job.jobPosition}
+                        description={job.jobDescription}
                     />
                 </List.Item>
             )}
